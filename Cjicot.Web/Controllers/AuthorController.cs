dc209d6c0519e0d1553430db23c80e57845174c3
OLAMILEKAN KAREEM <elkaysoft@outlook.com>
2021-12-30 02:51:12 +0100

    Add project files.

---
+++ Cjicot.Web/Controllers/AuthorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cjicot.Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult dashboard()
+        {
+            return View();
+        }
+    }
+}
