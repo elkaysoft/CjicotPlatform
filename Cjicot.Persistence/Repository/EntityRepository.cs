cc2e0ab1132d47488c57ad7813066820058999e0
OLAMILEKAN KAREEM <elkaysoft@outlook.com>
2022-01-02 23:32:18 +0100

    update

---
+++ Cjicot.Persistence/Repository/EntityRepository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cjicot.Persistence.Repository
+{
+    public class EntityRepository<T> : IRepository<T> where T : class
+    {
+        private CjcotContext _context;
+        private DbSet<T> dbSet;
+
+
+        public EntityRepository(CjcotContext cjcotContext)
+        {
+            _context = cjcotContext;
+            dbSet = _context.Set<T>();
+        }
+
+        public int Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            return _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression)
+        {
+            return dbSet.Where(expression).ToListAsync();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return dbSet.ToListAsync();
+        }
+
+        public IQueryable<T> GetAndInclude(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (includeProperties != null)
+            {
+                query = includeProperties.Aggregate(query,
+                          (current, include) => current.Include(include));
+            }
+
+            return query;
+        }
+
+        public T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T GetFirstOrDefault(Func<T, bool> predicate)
+        {
+            return dbSet.FirstOrDefault(predicate);
+        }
+
+        public T GetLastOrDefault(Func<T, bool> predicate)
+        {
+            return dbSet.LastOrDefault(predicate);  
+        }
+
+        public T GetSingleOrDefault(Func<T, bool> predicate)
+        {
+            return dbSet.SingleOrDefault(predicate);
+        }
+
+        public int Insert(T entity)
+        {
+            dbSet.Add(entity);
+            return _context.SaveChanges();
+        }
+
+        public int InsertRange(IEnumerable<T> entities)
+        {
+            dbSet.AddRange(entities);
+            return _context.SaveChanges();
+        }
+
+        public int Update(T entity)
+        {
+            dbSet.Update(entity);
+            return _context.SaveChanges();
+        }
+    }
+}
--- Cjicot.Persistence/Repository/IRepository.cs
+++ Cjicot.Persistence/Repository/IRepository.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cjicot.Persistence.Repository
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
-
+        T GetSingleOrDefault(Func<T, bool> predicate);
+        T GetLastOrDefault(Func<T, bool> predicate);
+        T GetFirstOrDefault(Func<T, bool> predicate);
+        T GetByID(object id);
+        IEnumerable<T> GetAll();
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAllAsync();
+        int Update(T entity);
+        int Insert(T entity);
+        int InsertRange(IEnumerable<T> entities);
+        int Delete(T entity);       
+        IQueryable<T> GetAndInclude(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] includeProperties);        
     }
 }
