de6bf58ec8b5149783fcf8f1682727cd1d501a1c
OLAMILEKAN KAREEM <elkaysoft@outlook.com>
2022-01-02 23:43:06 +0100

    last update

---
+++ Cjicot.BusinessLogic/Utility/helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cjicot.Presentation.Utility
+{
+    public class helper
+    {
+        public static string EncryptPassword(string plainText)
+        {
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < bytes.Length; i++)
+                {
+                    sb.Append(bytes[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
